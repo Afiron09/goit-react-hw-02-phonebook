045e272ef8b65fb63c0fc6c000f307328dd65f49
Afiron09 <comixgod@gmail.com>
2024-01-11 03:37:08 +0200

    5

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+// import { nanoid } from 'nanoid'
+
+// model.id = nanoid() //=> "V1StGXR8_Z5jdHi6B-myT"
+export class App extends Component {
+  render() {
+    return <div>App</div>;
+  }
+}
